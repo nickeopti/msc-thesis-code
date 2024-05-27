4f83c9dbfa7c1978073e8a7d78e783cddf4db63f
Nicklas Boserup <nibos@di.ku.dk>
2024-05-27 09:58:28 +0000

    Visualise butterfly landmark data

---
+++ thesis/scripts/butterfly.py
@@ -0,0 +1,33 @@
+import argparse
+import os.path
+import sys
+
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import pandas as pd
+import selector
+
+parser = argparse.ArgumentParser(allow_abbrev=False)
+
+data_path = selector.get_argument(parser, 'data_path', type=str)
+metadata = pd.read_csv(os.path.join(data_path, 'metadata.txt'), sep=';')
+landmarks = pd.read_csv(os.path.join(data_path, 'aligned.txt'), sep=',', header=None)
+
+species = selector.get_argument(parser, 'species', type=str, default=None)
+
+if species is None:
+    print(metadata['species'])
+    sys.exit(0)
+
+a = jnp.array(landmarks.loc[metadata['species'] == species])[0].reshape((-1, 2))
+
+every = selector.get_argument(parser, 'every', type=int, default=1)
+
+hide_axes = selector.get_argument(parser, 'hide_axes', type=bool, default=False)
+
+plt.scatter(a[::every, 0], a[::every, 1], color='red')
+if hide_axes:
+    plt.axis('off')
+plt.gca().set_aspect('equal')
+plt.tight_layout()
+plt.savefig('butterfly.png', dpi=600)
